7c02be8f78b22fcc1bc15736849eb9873275089c
abkazan <71850041+abkazan@users.noreply.github.com>
2023-09-12 14:43:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => res.send({"message": "hello fucker"}));
 
