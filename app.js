6acf9c1588a864a9daf26dda874eb5476fe98645
abkazan <71850041+abkazan@users.noreply.github.com>
2023-09-12 15:55:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 const serviceAccount = require('/etc/secrets/FIREBASE_ENV.json');
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.STORAGE_LINK,
+});
+
+app.get("/api", (req, res) => {
+    const db = admin.firestore();
+    const docRef = db.collection('test').doc('testData');
+
+    docRef.get()
+        .then((doc) => {
+            if (doc.exists) {
+                const data = doc.data();
+                res.json({ "data": data });
+            } else {
+                res.json({ "data": [] });
+            }
+        })
+        .catch((error) => {
+            console.log('Error getting document:', error);
+            res.status(500).json({ "error": "Internal Server Error" });
+        });
+
+});
+
+
 
 app.get("/", (req, res) => res.send({"message": "hello fucker"}));
 
