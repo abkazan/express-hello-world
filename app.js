4ff9d671fed48eec82c3bee7ec5de16b671ec43d
abkazan <71850041+abkazan@users.noreply.github.com>
2023-09-12 14:29:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send({"message": "hello world"}));
+app.get("/", (req, res) => res.send({"message": "hello fucker"}));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
