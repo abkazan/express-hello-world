1235af33612ddc1a6851aef3381d2f1cb56235cc
abkazan <71850041+abkazan@users.noreply.github.com>
2023-09-12 15:44:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const serviceAccount = require('FIREBASE_ENV.json');
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,6 +14,8 @@ app.get("/", (req, res) => res.send({"message": "hello fucker"}));
 const test = process.env.TEST;
 app.get('/test', (req, res) => {
   res.send({"testEnv": test});
+  console.log(typeof serviceAccount);
+  console.log(serviceAccount);
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
