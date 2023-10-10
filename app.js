47a61fe44b421f338c31832bc55d54912b936aad
abkazan <71850041+abkazan@users.noreply.github.com>
2023-10-09 23:05:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const sharp = require('sharp');
 app.use(bodyParser.json());
 
 const serviceAccount = require('/etc/secrets/FIREBASE_ENV.json');
+
+app.get('/testing69', function (req, res) {
+    const userIP = req.ip;
+    res.json(userIP);
+    /* res.send('Hello World!'); */
+});
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
