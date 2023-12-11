e34bb06dffa785a198813e6263b976cced442335
abkazan <abkazan@wisc.edu>
2023-12-11 15:15:33 -0600

    once again debugging bucksin6ix comment endpoint

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ app.post('/bucksin6ix/comment', (req, res) => {
     let { message, title, episode, } = req.body;
     console.log('data recieved: ', req.body);
     let subject = '';
-    if (title === '') {
+    if (title === '' || title === undefined || title === null) {
         subject = 'New Comment from contact page';
     } else {
         subject = `New Comment on episode #${episode}, ${title}`;
